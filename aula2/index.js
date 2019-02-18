8f4aac7b625b1e4aa08aae9d112b5d537dfdb52c
Jailton Ventura <jottha_2006@hotmail.com>
2019-02-18 01:08:56 -0300

    Árvore

---
+++ aula2/index.js
@@ -0,0 +1,13 @@
+console.log('Hello World');
+
+//Árvore de Natal em ASCII
+const NUM_LINHAS = 10;
+
+for(let i = 0; i < NUM_LINHAS; i++) {
+    let espacos = ' '.repeat(NUM_LINHAS - i);
+    let conteudo = 'X'.repeat(i);
+    console.log(espacos + conteudo + 'X' + conteudo);
+}
+
+console.log((' '.repeat(NUM_LINHAS) + 'X' + '\n').repeat(4));
+//Pelo menos é um Hello World diferente
